a496517d4e5239a842fdd4ed8e5dc62d08c0e612
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-06-15 16:02:07 +0200

    aggiunti validator

---
+++ src/main/java/com/example/demo/validator/TrainerValidator.java
@@ -0,0 +1,30 @@
+package com.example.demo.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.example.demo.model.Trainer;
+import com.example.demo.service.TrainerService;
+
+@Component
+public class TrainerValidator implements Validator {
+
+	@Autowired
+	private TrainerService ts;
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return Trainer.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+//		if(this.cs.alreadyExists((Chef) target)) {
+//			errors.reject("chef.duplicato");
+//		}
+	}
+
+}
