b3917f0f052a07156bbb2ec2ebeeb61ba3f4d473
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-06-15 14:54:20 +0200

    aggiunti repository

---
+++ src/main/java/com/example/demo/repository/IndirizzoRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Indirizzo;
+
+public interface IndirizzoRepository extends CrudRepository<Indirizzo, Long> {
+
+}
---
+++ src/main/java/com/example/demo/repository/SalaRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Sala;
+
+public interface SalaRepository extends CrudRepository<Sala, Long> {
+
+}
