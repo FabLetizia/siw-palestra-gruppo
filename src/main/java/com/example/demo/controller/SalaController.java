db09bf6082219256dca5b69bba948a2ebf0d7602
FabLetizia <fabio.letizia01@gmail.com>
2022-06-17 12:47:17 +0200

    Update SalaController.java

--- src/main/java/com/example/demo/controller/SalaController.java
+++ src/main/java/com/example/demo/controller/SalaController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.model.Sala;
 import com.example.demo.service.SalaService;
 
 @Controller
@@ -10,4 +15,12 @@ public class SalaController {
 
 	@Autowired
 	private SalaService salaService;
+	
+	/* TODO */
+	@GetMapping("/admin/sale")
+	public String getSale(Model model) {
+		List<Sala> sale = salaService.findAll();  
+		model.addAttribute("sale", sale);
+		return "admin/sala/sale.html";
+	}
 }
