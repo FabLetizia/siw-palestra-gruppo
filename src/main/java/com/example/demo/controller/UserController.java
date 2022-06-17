87f2df686f4132ecd8b45d30fb5b9658e71d5e14
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-06-17 19:47:15 +0200

    aggiunte pagine user
    modificate pagine user trainer e corso, e anche
    UserController

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,85 +1,60 @@
-//package com.example.demo.controller;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//
-//import com.example.demo.model.Buffet;
-//import com.example.demo.model.Chef;
-//import com.example.demo.model.Ingrediente;
-//import com.example.demo.model.Piatto;
-//import com.example.demo.service.BuffetService;
-//import com.example.demo.service.ChefService;
-//import com.example.demo.service.PiattoService;
-//
-//@Controller
-//public class UserController {
-//	@Autowired
-//	private ChefService chefService;
-//	
-//	@Autowired
-//	private BuffetService buffetService;
-//	
-//	@Autowired
-//	private PiattoService piattoService;
-//	
-//	@GetMapping("/user/chefs")
-//	public String getChefs(Model model) {
-//		List<Chef> chefs = chefService.findAll();  
-//		model.addAttribute("chefs", chefs);
-//		return "user/userChefs.html";
-//	}
-//	
-//	@GetMapping("/user/buffets")
-//	public String getBuffets(Model model) {
-//		List<Buffet> buffets = buffetService.findAll();
-//		model.addAttribute("buffets", buffets);
-//		return "user/userBuffets.html";
-//	}
-//	
-//	@GetMapping("/user/chef/{id}")
-//	public String getChefById(@PathVariable("id") Long id, Model model) {
-//		Chef chef = chefService.findById(id);
-//		model.addAttribute("chef", chef);
-//		model.addAttribute("buffets", chef.getBuffet());
-//		return "user/userChef.html";
-//	}
-//	
-//	@GetMapping("/user/chef/buffet/{id}")
-//	public String getBuffetDiChef(@PathVariable("id") Long id, Model model) {
-//		Buffet buffet = buffetService.findById(id);
-//		model.addAttribute("buffet", buffet);
-//		model.addAttribute("piatti", buffet.getPiatti());
-//		return "user/userBuffet.html";
-//	}
-//	
-//	@GetMapping("/user/piatto/buffet/{id}")
-//	public String getIngredientiPerPiattoInBuffet(@PathVariable("id") Long id, Model model) {
-//		Piatto piatto = piattoService.findById(id);
-//		List<Ingrediente> ingredienti = piatto.getIngredienti();
-//		model.addAttribute("piatto", piatto);
-//		model.addAttribute("ingredienti", ingredienti);
-//		return "user/userIngredientiPerPiatto.html";
-//	}
-//	
-//	@GetMapping("/user/buffet/chef/{id}")
-//	public String getChef(@PathVariable("id") Long id, Model model) {
-//		Chef chef = chefService.findById(id);
-//		List<Buffet> buffets = chef.getBuffet();
-//		model.addAttribute("chef", chef);
-//		model.addAttribute("buffets", buffets);
-//		return "user/userChef.html";
-//	}
-//	
-//	@GetMapping("/user/buffet/{id}")
-//	public String getBuffet(@PathVariable("id") Long id, Model model) {
-//		Buffet buffet = buffetService.findById(id);
-//		model.addAttribute("buffet", buffet);
-//		model.addAttribute("piatti", buffet.getPiatti());
-//		return "user/userBuffet.html";
-//	}
-//}
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.model.Corso;
+import com.example.demo.model.Trainer;
+import com.example.demo.service.CorsoService;
+import com.example.demo.service.TrainerService;
+
+
+
+@Controller
+public class UserController {
+
+	@Autowired
+	private TrainerService trainerService;
+	
+	@Autowired
+	private CorsoService corsoService;
+	
+	@GetMapping("/user/trainers")
+	public String getTrainers(Model model) {
+		List<Trainer> trainers = trainerService.findAll();  
+		model.addAttribute("trainers", trainers);
+		return "user/userTrainers.html";
+	}
+	
+	@GetMapping("/user/corsi")
+	public String getCorsi(Model model) {
+		List<Corso> corsi = corsoService.findAll();
+		model.addAttribute("corsi", corsi);
+		return "user/userCorsi.html";
+	}
+	
+	@GetMapping("/user/trainer/{id}")
+	public String getTrainerById(@PathVariable("id") Long id, Model model) {
+		Trainer trainer = trainerService.findById(id);
+		model.addAttribute("trainer", trainer);
+		return "user/userTrainer.html";
+	}
+	
+	
+	@GetMapping("/user/corso/{id}")
+	public String getCorso(@PathVariable("id") Long id, Model model) {
+		Corso corso = corsoService.findById(id);
+		model.addAttribute("corso", corso);
+		String nomeSala = "ancora nessuna sala";
+		if(corsoService.hasSalaNull(corso))
+			model.addAttribute("sala", nomeSala);
+		else
+			model.addAttribute("sala", corso.getSala().getNome());
+		return "user/userCorso.html";
+	}
+}
