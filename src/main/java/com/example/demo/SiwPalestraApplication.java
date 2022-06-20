8d8ef75fbc27d3423d540135f8f3283b3c5c6025
FabLetizia <fabio.letizia01@gmail.com>
2022-06-20 11:55:45 +0200

    Serve per il deploy
    Co-Authored-By: Elisa Catena <93096381+elisacatena@users.noreply.github.com>

--- src/main/java/com/example/demo/SiwPalestraApplication.java
+++ src/main/java/com/example/demo/SiwPalestraApplication.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.demo.model.Credentials;
+import com.example.demo.model.User;
+import com.example.demo.repository.CredentialsRepository;
 
 @SpringBootApplication
 public class SiwPalestraApplication implements CommandLineRunner {
 	
-//    @Autowired private CredentialsRepository credR;
-//	@Autowired protected PasswordEncoder passwordEncoder;
+    @Autowired private CredentialsRepository credR;
+	@Autowired protected PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiwPalestraApplication.class, args);
@@ -16,17 +22,17 @@ public class SiwPalestraApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-//		User u = new User();
-//		u.setNome("Fabio");
-//		u.setCognome("Letizia");
-//		
-//		Credentials c = new Credentials();
-//		c.setUser(u);
-//		c.setPassword(this.passwordEncoder.encode("admin"));
-//		c.setUsername("admin");
-//		c.setRole("ADMIN");
-//		
-//		credR.save(c);	
+		User u = new User();
+		u.setNome("Fabio");
+		u.setCognome("Letizia");
+		
+		Credentials c = new Credentials();
+		c.setUser(u);
+		c.setPassword(this.passwordEncoder.encode("admin"));
+		c.setUsername("admin");
+		c.setRole("ADMIN");
+		
+		credR.save(c);	
 	}
 
 }
